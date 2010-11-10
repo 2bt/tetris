82541927a99453aaad88096e3f34094b73802e66
twobit <twobit@gmx.net>
2010-11-10 23:48:53 +0100

    made smaller

--- tetris.lua
+++ tetris.lua
@@ -1,14 +1,9 @@
 #!/usr/bin/lua
-math.randomseed(os.time())
-R,E=math.random,os.execute
-E("stty cbreak -echo min 0")
-print("\027[?25l\027[2J")
-f={}z,t=0,0
+math.randomseed(os.time())R,E=math.random,os.execute
+E("stty cbreak -echo min 0")print("\027[?25l\027[2J")f={}t,z,u=0,0,1
 for i=1,265,12 do f[i]=3 f[i+11]=3 end
 for i=266,275 do f[i]=3 end
-repeat
-	if not a then
-		repeat
+repeat if not a then repeat
 			o,p=p,
 			({{0,0,0,0,0,15,15,0,0,15,15},
 			{0,0,10,0,5,5,15,5,0,0,10,0,0,0,10},
@@ -23,25 +18,20 @@ repeat
 		for i=1,16 do a[math.floor((i-1)/4)*12+(i-1)%4+5]=o[i]end
 	end
 	if c then
-		print("\027[2;27HLines:", z, "\027[4;27HNext:")
 		for i=1,16 do
-			if i%4==1 then
-				io.write("\027["..6+math.floor(i/4)..";27H")
-			end
-			io.write((p[i]and p[i]/g%2>=1 and"\027[44m"or"").."  \027[m")
+			if i%4==1 then io.write("\027[",2+math.floor(i/4),";27H")end
+			io.write(p[i]and p[i]/g%2>=1 and"\027[44m"or"","  \027[m")
 		end
+		print("\027[7;27HLevel: ",u,"\027[9;27HLines:",z)
 		io.write("\027[;H")
 		for i=25,276 do
-			io.write((f[i]and"\027["..44+f[i].."m"or
-				a[i]and a[i]/r%2>=1 and"\027[44m"or"").."  \027[m")
+			io.write(f[i]and"\027["..44+f[i].."m"or a[i]and a[i]/r%2>=1 and"\027[44m"or"","  \027[m")
 			if i%12==0 then print()end
 		end
 	else E("sleep .01")t=t+1 end
-	e=c=="q"
+	e=c=="q"h,s,b=0,r,a
 	if c==" "then E("sleep .005")else c=io.read(1)end
-	h,s,b=0,r,a
-	if c=="y"or c=="A"then r=r<2 and 8 or r/2 end
-	if c=="x"then r=r*2%15 end
+	r=c=="x"and r*2%15 or(c=="y"or c=="A")and(r<2 and 8 or r/2)or r
 	if c=="B"or c==" "or t>50 then t,h,c=0,12,c or 0 end
 	a,l={},a
 	for i,q in pairs(l)do a[i+(c=="C"and 1 or 0)-(c=="D"and 1 or 0)+h]=q end
@@ -52,7 +42,7 @@ repeat
 				E("sleep .2")
 				repeat c=io.read(1)until not c
 				for i,q in pairs(a)do
-					if i<25 then c="q"end
+					c=i<25 and "q"or c
 					f[i]=f[i]or q/r%2>=1 and 0
 				end
 				a=nil
@@ -71,5 +61,4 @@ repeat
 		end
 	end
 until e
-print("Game Over.\027[?25h")
-E("stty sane")
+print("Game Over.\027[?25h")E("stty sane")
